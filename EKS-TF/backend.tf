0630997bdad281e16ce7cd560c1eef561b5aa8e1
Shri3010 <146299718+Shri3010@users.noreply.github.com>
2024-01-08 20:43:43 -0500

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "karthikbucket3010" # Replace with your actual S3 bucket name
+    bucket = "my-sbks3-bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 }
